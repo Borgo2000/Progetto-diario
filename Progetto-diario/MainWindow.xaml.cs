45ad0dadfbffae031c732519bb1105027596e05d
borgo-sauro <mattiaborgo08@gmail.com>
2026-01-14 21:36:53 +0100

    parte grafica

--- Progetto-diario/MainWindow.xaml.cs
+++ Progetto-diario/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.VisualBasic;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -21,11 +23,30 @@ namespace Progetto_diario
     public partial class MainWindow : Window
     {
         //private Applicazione app = null;
+        
         public MainWindow()
         {
 
             InitializeComponent();
-            ////app = new Applicazione();
+
+
+            //DiaryGrid.Children.Add(CreateDiaryCard("../../diario1.png", "Bloccato"));
+            //DiaryGrid.Children.Add(CreateDiaryCard("../../diario2.png", "Sbloccato"));
+            //DiaryGrid.Children.Add(CreateDiaryCard("../../diario3.png", "Bloccato"));
+            //DiaryGrid.Children.Add(CreateDiaryCard("../../diario7.png", "Sbloccato"));
+            //app = new Applicazione();
+            //ReadOnlyCollection<InfoDiario> diari =app.getDiari();
+            //foreach (InfoDiario diario in diari)
+            //{
+            //    string imagePath = diario.getPercorso()+"diario1.png";
+            //    string labelText = diario.getNome();
+            //    DiaryGrid.Children.Add(CreateDiaryCard(imagePath, labelText));
+
+            //}
+
+
+
+
 
         }
 
@@ -34,6 +55,87 @@ namespace Progetto_diario
             FinestraDiario finestraDiario = new FinestraDiario();
             finestraDiario.ShowDialog();
         }
+        //private Grid CreateDiaryCard(string imagePath, string labelText)
+        //{
+        //    // Grid container
+        //    Grid grid = new Grid
+        //    {
+        //        Width = 148,
+        //        Height = 170
+        //    };
+
+        //    // Rectangle con immagine
+        //    Rectangle rect = new Rectangle
+        //    {
+        //        Width = 148,
+        //        Height = 134,
+        //        Style = (Style)FindResource("DiaryCardStyle")
+        //    };
+
+        //    rect.SetValue(Panel.ZIndexProperty, 1);
+        //    rect.Fill = new ImageBrush
+        //    {
+        //        ImageSource = new BitmapImage(new Uri(imagePath, UriKind.Relative)),
+        //        Stretch = Stretch.Uniform
+        //    };
+
+        //    // Label
+        //    Label label = new Label
+        //    {
+        //        Content = labelText,
+        //        FontWeight = FontWeights.Bold,
+        //        FontSize = 14,
+        //        Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#B0B3C4")),
+        //        Background = Brushes.Transparent,
+        //        HorizontalAlignment = HorizontalAlignment.Center,
+        //        VerticalAlignment = VerticalAlignment.Bottom,
+        //        Margin = new Thickness(0, 0, 0, 12)
+        //    };
+
+        //    label.SetValue(Panel.ZIndexProperty, 999);
+
+        //    // Stile con trigger
+        //    Style style = new Style(typeof(Label));
+        //    style.Setters.Add(new Setter(Label.VisibilityProperty, Visibility.Collapsed));
+
+        //    DataTrigger trigger = new DataTrigger
+        //    {
+        //        Binding = new Binding("IsMouseOver")
+        //        {
+        //            RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(Grid), 1)
+        //        },
+        //        Value = true
+        //    };
+        //    trigger.Setters.Add(new Setter(Label.VisibilityProperty, Visibility.Visible));
+
+        //    style.Triggers.Add(trigger);
+        //    label.Style = style;
+
+        //    // Aggiunta elementi alla grid
+        //    grid.Children.Add(rect);
+        //    grid.Children.Add(label);
+
+        //    return grid;
+        //}
+        //private void img_button_aggiungi_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    //app.aggiungiDiario("NuovoDiario", DateTime.Now);
+        //    //string nomeDiario = Interaction.InputBox(
+        //    //    "Inserisci il nome del nuovo diario:",
+        //    //    "Nuovo Diario",
+        //    //    ""
+        //    //);
+
+        //    //if (!string.IsNullOrWhiteSpace(nomeDiario))
+        //    //{
+        //    //    MessageBox.Show($"Hai creato il diario: {nomeDiario}");
+        //    //    // qui puoi aggiungere il diario alla tua UniformGrid
+        //    //}
+
+        //}
+       
+
+
 
 
         //public void apriDiario(object sender, RoutedEventArgs e)
